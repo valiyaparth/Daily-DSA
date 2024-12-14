7a25e8748c12cc8f7cf6e708da88fda08172901f
Parth Valiya <valiyaparth1407@gmail.com>
2024-12-14 14:39:45 +0530

    Initial Commit

---
+++ Array/Remove_duplicate.java
@@ -0,0 +1,13 @@
+class Remove_duplicate {
+    public int removeDuplicates(int[] nums) {
+        int k=1;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i] != nums[i+1]){
+                nums[k]=nums[i+1];
+                k++;
+
+            }
+        }
+        return k;
+    }
+}
